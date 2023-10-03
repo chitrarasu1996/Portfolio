33c9df737380102839de74b60bc96d68534c9db7
A.chitrarasu <chitrarasumit@gmail.com>
2023-10-03 17:35:31 +0530

    resume updated

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -3,7 +3,7 @@ import { FaGithub } from 'react-icons/fa'
 
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import {AiOutlineLinkedin} from "react-icons/ai"
-import resume from "../details/edit.pdf"
+import resume from "../details/resume.pdf"
 const SocialLinks = () => {
     const links = [
      
