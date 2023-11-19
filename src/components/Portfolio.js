0966d33f3e8a99bcf7412106ed04779eac7d230c
A.chitrarasu <chitrarasumit@gmail.com>
2023-11-19 14:11:00 +0530

    updated projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,8 +3,10 @@ import flipkart from "../assets/flipkart.jpg"
 import chatapp from "../assets/chatappimg.webp"
 import gmail from "../assets/g-mailclone.jpg"
 import webscrap from "../assets/webscraping.jpg"
+import noteimg from "../images/noteimg.webp"
 const Portfolio = () => {
     const projectImages=[
+    
         {
             id:1,
             alt:"e-commerce",
@@ -37,6 +39,17 @@ const Portfolio = () => {
         }
     ,
     {
+      id:5,
+      tittle:"note-taking-app",
+      alt:"note-taking-app",
+      style:"20px",
+      src: noteimg,
+      target:"_blank",
+      stack:"Notes taking application Project with MERN Stack",
+      ref:"https://notes-taking-application-frontend.netlify.app/",
+      code:'https://github.com/chitrarasu1996/notes-taking-application-frontend.git'
+    },
+    {
         id:4,
         alt:"web-scraping",
         tittle:"web-scraping",
@@ -46,7 +59,9 @@ const Portfolio = () => {
         ref:"https://web-scraping-123.netlify.app/",
         code:"https://github.com/chitrarasu1996/frontend-webscraping.git"
         
-    }
+    },
+
+ 
     ]
   return (
 <div name="projects" style={{height:"100%"}}  className='px-32 sm:px-52 py-32 h-full w-full
@@ -63,7 +78,7 @@ some of my work here
         </div>
 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8
  sm:px-0'>
-    {projectImages.map(({id,src,ref,code,tittle,alt,target,stack})=>(
+    {projectImages.map(({id,src,ref,code,tittle,alt,target,stack,style})=>(
         <div style={{width:"210px"}} key={id} className=' shadow-md shadow-gray-600 rounded-lg'>
         <img
   
@@ -71,8 +86,8 @@ some of my work here
       <a href={ref} rel='noreferrer' target="_blank"> <p className='text-center mt-2 cursor-pointer duration-105 hover:scale-105 py-1 font-bold'>{tittle}</p></a>
       
        <div className='ms-1 ps-2 pe-1' style={{maxHeight:"80px"}}><span className='font-semibold'>Tech Stack : </span> {stack}</div>
-        <div className='flex items-center justify-center'>
-        <a href={ref} target={target}><button className='duration-105 hover:scale-105 w-1/2 px-4 py-6 mx-3'>Demo</button></a>  
+        <div  className={`flex items-center justify-center ${id===5?"p-3":""}`}>
+        <a href={ref} target={target} ><button className='duration-105 hover:scale-105 w-1/2 px-4 py-6 mx-3'>Demo</button></a>  
        <a href={code} target={target}> <button className='duration-105 hover:scale-105 w-1/2 px-4 py-6 mx-3'>Code</button> </a>
         </div>
       
